3c45642b64ca426d5faf8716e9ce45609c74c8dd
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:20:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,31 +1,6 @@
 def roll_call_dwarves(dwarves)
-index = 0
-dwarves.each {|dwarve|
-    puts  "#dwarve", "blach"  }
-index += 1
-end
-
-
-
-
-def roll_call_dwarves(dwarves)
- dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
-end
-
-end
-roll_call_dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-
-
-
-
-def reverse_each_word(sentence)
-  new_sentence = []
-sentence.split.each {|word|
-     new_sentence  << word.reverse}
-new_sentence.join(" ")
-end
-
+  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end	end
 
 
 def summon_captain_planet# code an argument here
@@ -40,3 +15,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+learn
