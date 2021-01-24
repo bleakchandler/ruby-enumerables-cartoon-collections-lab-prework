ffdc6becd556832e1bf6bc8bd625b976a1a2e395
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 21:27:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 
 
 
-def long_planteer_calls(planeteer_calls)
+def long_planeteer_calls(planeteer_calls)
 
   planeteer_calls.each {
     |index| if index.length >= 4 
@@ -37,7 +37,7 @@ def long_planteer_calls(planeteer_calls)
 end
 
 
-long_planeteer_calls short_words = ["puff", "go", "two"]
+long_planeteer_calls planeteer_calls = ["puff", "go", "two"]
 
 
 
