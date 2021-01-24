f5cc91fa4b095c44828756c462591886e02d98ce
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:50:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  dwarves.each {
-    |dwarf, num| puts "#{planeteer_calls}!"
+  planeteer_calls.each {
+    |planeteer| puts "#{planeteer}!"
   }
 end
 
