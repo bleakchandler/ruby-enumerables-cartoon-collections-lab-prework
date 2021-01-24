0c0c54652c2e5c9c96c6a97a99d229b0d2c83f11
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:48:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,15 +10,13 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  order = 1
   dwarves.each {
-    |dwarf, num| puts "#{order}. #{dwarf}"
-  order += 1
+    |dwarf, num| puts "#{planeteer_calls}!"
   }
 end
 
 
-end
+
 
 
 
