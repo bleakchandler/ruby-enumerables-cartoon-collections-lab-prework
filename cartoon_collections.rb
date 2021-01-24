e560d00308a463ce9ca309d90ad2957c233ac7d3
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:47:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,14 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  
+  order = 1
+  dwarves.each {
+    |dwarf, num| puts "#{order}. #{dwarf}"
+  order += 1
+  }
+end
+
+
 end
 
 
@@ -19,3 +26,9 @@ def long_planteer_calls(planeteer_calls)
   
 end
 
+
+
+
+def find_the_cheese(snacks)
+  
+end
