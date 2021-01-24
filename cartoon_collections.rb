f149f37429e15c384127ac0389474a3995999477
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:20:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(dwarves)
-  # Your code here	  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
-end	end
+def roll_call_dwarves(dwarves)   dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"} end
 
 def summon_captain_planet# code an argument here
   # Your code here
