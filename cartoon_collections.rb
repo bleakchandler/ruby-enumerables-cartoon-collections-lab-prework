34bea8e7c70110a01562d75e44a7123ef84918bc
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:46:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)
-  num_order = 1
+  order = 1
   dwarves.each {
-    |dwarf, num| puts "#{num_order}. #{dwarf}"
-  new_word += 1
+    |dwarf, num| puts "#{order}. #{dwarf}"
+  order += 1
   }
   
 end
