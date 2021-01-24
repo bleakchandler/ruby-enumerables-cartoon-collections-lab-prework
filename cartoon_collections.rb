84d594935dd5617ec52137e2af13a0e53eadb55e
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:19:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,11 @@ index += 1
 end
 
 
+
+
+def roll_call_dwarves(dwarves)
+  # Your code here	  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end	end
 roll_call_dwarves = ["Dopey", "Grumpy", "Bashful"]
 
 
