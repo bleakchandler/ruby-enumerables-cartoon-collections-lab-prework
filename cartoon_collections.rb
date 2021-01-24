f47dacb23ac07ef63dad7b6bc8e9dbb6b2ce413c
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 20:59:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,15 +16,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def roll_call_dwarves(dwarves)
-  order = 1
-  dwarves.each {
-    |dwarf, num| puts "#{order}. #{dwarf}"
-  order += 1
-  }
-end
 
-roll_call_dwarves dwarves =  ["carrot", "cucumber", "pepper"]
+summon_captain_planet planeteer_calls =  ["carrot", "cucumber", "pepper"]
 
 
 def long_planteer_calls(planeteer_calls)
